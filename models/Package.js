dc201c920f4557e96275b470c002493a93b77ed1
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-07-28 21:30:37 +0530

    add desc col to package model

--- models/Package.js
+++ models/Package.js
@@ -14,6 +14,10 @@ const PackageSchema = new Schema(
       type: Number,
       required: true,
     },
+    description: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
