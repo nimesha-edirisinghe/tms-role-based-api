e5300d71c2d1849cacaa8c38491c074f2751cc77
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-08-19 20:48:48 +0530

    seeder implementation v1

--- seeder.js
+++ seeder.js
@@ -2,3 +2,8 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const colors = require("colors");
 const dotenv = require("dotenv");
+
+//Load env variables
+dotenv.config({ path: "./.env" });
+// Load models
+const User = require("./models/User");
